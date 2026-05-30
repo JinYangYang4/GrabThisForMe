348ed55427fd5123a4f10525604cec367b54f916
jinyangyang <1401495628@qq.com>
2026-05-30 17:33:33 +0800

    美化了“我的”页面

--- app/src/main/java/com/example/grabthisforme/activity/myfragment/view/FragmentMy.kt
+++ app/src/main/java/com/example/grabthisforme/activity/myfragment/view/FragmentMy.kt
@@ -65,6 +65,15 @@ class FragmentMy : Fragment() {
         binding.tvEdit.setOnClickListener {
             (requireActivity() as MainActivity).intentToMiscFragment(R.id.action_blankFragment_to_personalInformation)
         }
+        binding.quickPost.setOnClickListener {
+            (requireActivity() as MainActivity).intentToMiscFragment(R.id.action_blankFragment_to_fragmentMyStore)
+        }
+        binding.quickPost.setOnClickListener {
+            (requireActivity() as MainActivity).intentToMiscFragment(R.id.action_blankFragment_to_postTopicFragemnt)
+        }
+        binding.quickCoupon.setOnClickListener {
+            (requireActivity() as MainActivity).intentToMiscFragment(R.id.action_blankFragment_to_couponFragment)
+        }
 
     }
 
