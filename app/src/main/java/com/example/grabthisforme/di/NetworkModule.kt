8f206452ef6797af67a1a61db8ad3addd4c8ad0f
jinyangyang <1401495628@qq.com>
2026-07-06 19:19:21 +0800

    增加ACK回报错误补发

--- app/src/main/java/com/example/grabthisforme/di/NetworkModule.kt
+++ app/src/main/java/com/example/grabthisforme/di/NetworkModule.kt
@@ -24,7 +24,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
-    const val BASE_URL = "http://120.77.9.62:8080/"
+    const val BASE_URL =  "http://10.0.2.2:8080/"
 
     @Provides
     @Singleton
